6920978d79158799fcbd8b426fff0f15a35b2ed0
Felipe Vargas <felipe.vargas1922@gmail.com>
2022-04-25 17:51:24 -0300

    App v1.2

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,32 +1,51 @@
 import { StyleSheet, Text, View, Button } from 'react-native';
 import UserTextInput from '../components/UserTextInput';
-import React, { useLayoutEffect, useState } from 'react'
+import React, { useState } from 'react'
 
 export default function Home({ navigation }) {
 
-    const [quilometragem, setQuilometragem] = useState("")
-    const [gasolinaUsada, setGasolinaUsada] = useState("") 
-    const [resultado, setResultado] = useState(0)
+    const [quilometragem, setQuilometragem] = useState('')
+    const [gasolinaUsada, setGasolinaUsada] = useState('')
+    const [avisoValor, setAvisoValor] = useState("Informe os valores abaixo!")
+
+
+    const onPress = () => {
+
+        if (quilometragem > 0 && gasolinaUsada > 0)
+            navigation.navigate("Resultado", { paramKeyQuilometragem: quilometragem, paramKeyGasolina: gasolinaUsada })
+            setAvisoValor("Informe os valores abaixo")
+
+        if (quilometragem == "" || gasolinaUsada == "")
+            setAvisoValor("Adicione valores válidos!")
+
+        if (quilometragem == 0 && gasolinaUsada == 0)
+            setAvisoValor("Adicione valores válidos!")
+            setQuilometragem("")
+            setGasolinaUsada("")
+    }
 
     return (
         <View style={styles.container}>
+            <Text>
+                {avisoValor}
+            </Text>
+
             <UserTextInput
                 value={quilometragem}
                 set={setQuilometragem}
-                placeHolderText="Informe a distancia percorrida"
+                placeHolderText="Infome a distância percorrida em Km"
             />
 
             <UserTextInput
                 value={gasolinaUsada}
                 set={setGasolinaUsada}
-                placeHolderText="Informe a quantidade de combustivel usada"
+                placeHolderText="Informe o consumo de combustível em Litros"
             />
-            <View>
-                <Button
-                    title='Calcular'
-                    onPress={() => navigation.navigate("Resultado", { paramKey1: quilometragem, paramKey2: gasolinaUsada })}
-                />
-            </View>
+            <Button
+                title='Calcular'
+                color='#cccaca'
+                onPress={onPress}
+            />
         </View>
     );
 }
--- src/pages/Result.js
+++ src/pages/Result.js
@@ -1,40 +1,56 @@
-import { View, Text, Button, StyleSheet } from 'react-native'
-import React, { useState } from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import React, { useState, useLayoutEffect } from 'react'
 
 export default function Result({ route }) {
 
-  const [quilometragem, setQuilometragem] = useState(route.params.paramKey1)
-  const [gasolinaUsada, , setGasolinaUsada,] = useState(route.params.paramKey2)
+  const [quilometragem, setQuilometragem] = useState(route.params.paramKeyQuilometragem)
+  const [gasolinaUsada, , setGasolinaUsada,] = useState(route.params.paramKeyGasolina)
   const [resultado, setResultado] = useState(quilometragem / gasolinaUsada)
-  const [resultadoNivel, setResultadoNivel] = useState('')
+  const [resultadoNivel, setResultadoNivel] = useState(0)
+
+  useLayoutEffect(() => {
+    verificarNivelConsumo()
+  }, [])
 
   const verificarNivelConsumo = () => {
+    setResultado(quilometragem / gasolinaUsada)
+
     if (resultado >= 12)
       setResultadoNivel("Nivel A")
 
-    if (resultado > 10 && resultado <= 12)
+    if (resultado >= 10 && resultado < 12)
       setResultadoNivel("Nivel B")
 
-    if (resultado > 8 && resultado <= 10)
+    if (resultado >= 8 && resultado < 10)
       setResultadoNivel("Nivel C")
 
-    if (resultado > 4 && resultado <= 8)
+    if (resultado >= 4 && resultado < 8)
       setResultadoNivel("Nivel D")
 
-    if (resultado <= 4)
+    if (resultado < 4)
       setResultadoNivel("Nivel E")
   }
 
   return (
 
-    <View>
+    <View style={styles.container}>
 
-      <Text onLayout={verificarNivelConsumo}>
-        Consumo= {resultado}
+      <Text style={{ fontSize: 16}}>
+        Consumo= { parseFloat(resultado.toFixed(2)) } Km/l
         {"\n"}
-        Dados= {resultadoNivel}
+        Nivel de consumo= {resultadoNivel}
       </Text>
 
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+      padding: 10,
+      borderRadius: 10,
+      margin: 5,
+      backgroundColor: '#dbdbdb',
+      justifyContent: 'center'
+  }
+});
